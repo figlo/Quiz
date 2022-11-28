13f8491c03b9e4022652a6f27bcee39c549ca1ad
figlo <miro@figlar.sk>
2022-11-28 15:10:12 +0100

    setting up the question part 2

--- app/src/main/java/sk/figlar/quiz/Constants.kt
+++ app/src/main/java/sk/figlar/quiz/Constants.kt
@@ -7,7 +7,7 @@ object Constants {
 
         val question1 = Question(
             id = 1,
-            questionText = "What country does this flag belong to?",
+            text = "What country does this flag belong to?",
             image = R.drawable.ic_flag_of_argentina,
             options = listOf("Argentina", "Australia", "Armenia", "Austria"),
             correctAnswer = 0
@@ -16,7 +16,7 @@ object Constants {
 
         val question2 = Question(
             id = 2,
-            questionText = "What country does this flag belong to?",
+            text = "What country does this flag belong to?",
             image = R.drawable.ic_flag_of_belgium,
             options = listOf("Bahrain", "Burundi", "Belgium", "Bulgaria"),
             correctAnswer = 0
@@ -25,7 +25,7 @@ object Constants {
 
         val question3 = Question(
             id = 3,
-            questionText = "What country does this flag belong to?",
+            text = "What country does this flag belong to?",
             image = R.drawable.ic_flag_of_denmark,
             options = listOf("Norway", "Sweden", "Finland", "Denmark"),
             correctAnswer = 0
--- app/src/main/java/sk/figlar/quiz/Question.kt
+++ app/src/main/java/sk/figlar/quiz/Question.kt
@@ -2,7 +2,7 @@ package sk.figlar.quiz
 
 data class Question(
     val id: Int,
-    val questionText: String,
+    val text: String,
     val image: Int,
     val options: List<String>,
     val correctAnswer: Int,
