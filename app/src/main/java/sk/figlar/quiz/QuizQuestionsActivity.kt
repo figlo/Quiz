169f5f73fc44e634a64d6e443c92b8e4a1aa849a
figlo <miro@figlar.sk>
2022-11-28 20:17:56 +0100

    adding the button functionality

--- app/src/main/java/sk/figlar/quiz/QuizQuestionsActivity.kt
+++ app/src/main/java/sk/figlar/quiz/QuizQuestionsActivity.kt
@@ -1,12 +1,21 @@
 package sk.figlar.quiz
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionList: List<Question>? = null
+    private var mSelectedOption: Int = 0
 
     private var progressBar: ProgressBar? = null
     private var tvProgress: TextView? = null
@@ -17,6 +26,8 @@ class QuizQuestionsActivity : AppCompatActivity() {
     private var tvOptionTwo: TextView? = null
     private var tvOptionThree: TextView? = null
     private var tvOptionFour: TextView? = null
+
+    private var btnSubmit: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,19 +43,92 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tvOptionThree = findViewById(R.id.tvOptionThree)
         tvOptionFour = findViewById(R.id.tvOptionFour)
 
-        val questionsList = Constants.getQuestions()
+        btnSubmit = findViewById(R.id.btnSubmit)
 
-        var currentPosition = 1
-        var question = questionsList[currentPosition - 1]
+        tvOptionOne?.setOnClickListener(this)
+        tvOptionTwo?.setOnClickListener(this)
+        tvOptionThree?.setOnClickListener(this)
+        tvOptionFour?.setOnClickListener(this)
+        btnSubmit?.setOnClickListener(this)
+
+        mQuestionList = Constants.getQuestions()
+
+        setQuestion()
+    }
+
+    private fun defaultOptionsView() {
+        val options: MutableList<TextView> = mutableListOf()
+        options.add(tvOptionOne!!)
+        options.add(tvOptionTwo!!)
+        options.add(tvOptionThree!!)
+        options.add(tvOptionFour!!)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7a8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg,
+            )
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+        defaultOptionsView()
+
+        mSelectedOption = selectedOptionNum
+
+        tv.setTextColor(Color.parseColor("#363a43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg,
+        )
+    }
+
+    private fun setQuestion() {
+        var question = mQuestionList!![mCurrentPosition - 1]
         ivImage?.setImageResource(question.image)
-        progressBar?.progress = currentPosition
-        tvProgress?.text = "$currentPosition / ${progressBar?.max}"
+        progressBar?.progress = mCurrentPosition
+        tvProgress?.text = "$mCurrentPosition / ${progressBar?.max}"
         tvQuestion?.text = question.text
         tvOptionOne?.text = question.options[0]
         tvOptionTwo?.text = question.options[1]
         tvOptionThree?.text = question.options[2]
         tvOptionFour?.text = question.options[3]
 
+        if (mCurrentPosition == mQuestionList!!.size) {
+            btnSubmit?.text = "FINISH"
+        } else {
+            btnSubmit?.text = "SUBMIT"
+        }
+    }
 
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.tvOptionOne   -> {
+                tvOptionOne?.let {
+                    selectedOptionView(it, 0)
+                }
+            }
+            R.id.tvOptionTwo   -> {
+                tvOptionTwo?.let {
+                    selectedOptionView(it, 1)
+                }
+            }
+            R.id.tvOptionThree -> {
+                tvOptionThree?.let {
+                    selectedOptionView(it, 2)
+                }
+            }
+            R.id.tvOptionFour  -> {
+                tvOptionFour?.let {
+                    selectedOptionView(it, 3)
+                }
+            }
+            R.id.btnSubmit     -> {
+                // todo
+            }
+        }
     }
 }
